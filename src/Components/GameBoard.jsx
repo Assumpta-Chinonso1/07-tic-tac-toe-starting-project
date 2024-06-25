c221014fee7e960d294c557d767d13552a1be6ff
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-06-25 16:27:05 +0100

    Removed commented codes

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -4,20 +4,7 @@ import { useState } from "react"
 
  export default function GameBoard({onSelectPlayer, board}) {
 
-    //const [gameBoard, setGameBoard] = useState(initialGameBoard)
-
-    //function handleClickEdit(rowIndex, colIndex) {
-      //  setGameBoard((previousGame) => {
-
-         //   const updatedGame = [...previousGame.map((innerArray) =>[...innerArray])]
-           // updatedGame[rowIndex][colIndex] = activePlayerSymbol;
-           // return updatedGame
-       // })
-        
-      // onSelectPlayer ()
-
-    //}
-
+   
     return (
         <ol id="game-board">
 
